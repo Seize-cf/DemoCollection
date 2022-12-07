143674ae3f99c73feaa42d95801aea64effa4346
Gaotity <39005402+Seize-cf@users.noreply.github.com>
2022-12-07 17:20:22 +0800

    :tada: first commit

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import {terser} from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+/**
+ * @type { import('rollup').RollupOptions }
+ */
+export default {
+  input: ["src/index.js"],
+  output: [
+    {
+      dir: "dist/es",
+      format: "esm",
+      // 只有使用 Output 阶段相关钩子的插件才能够放到这个配置中
+      // 详见 https://github.com/rollup/awesome#output
+      plugins: [terser()],
+    },
+    {
+      dir: "dist/cjs",
+      format: "cjs",
+    },
+  ],
+  // 通过 plugins 参数添加插件
+  plugins: [resolve(), commonjs()],
+};
